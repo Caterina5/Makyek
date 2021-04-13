b03274e7d86b1fa2b5392ce604ac140130a9c813
kate <katerinagerace@hotmail.it>
2021-04-13 15:44:03 +0200

    primo commit

---
+++ view/Pedina.java
@@ -0,0 +1,51 @@
+package view;
+
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import model.Scacchiera;
+
+
+
+public class Pedina extends Casella {
+
+	
+	public Pedina(int valore) {
+		
+		try {
+			Image pedinaBianca = ImageIO.read(getClass().getResourceAsStream("/img/pedinaB.png"));
+			Image pedinaNera = ImageIO.read(getClass().getResourceAsStream("/img/pedinaN.png"));
+			Image damaBianca = ImageIO.read(getClass().getResourceAsStream("/img/damaB.png"));
+			Image damaNera = ImageIO.read(getClass().getResourceAsStream("/img/damaN.png"));
+
+			if(valore==1)
+			{
+				Icon icona = new ImageIcon(pedinaBianca);
+				this.setIcon(icona);
+			}
+			else if(valore==2){
+				Icon icona = new ImageIcon(pedinaNera);
+				this.setIcon(icona);	
+			}
+			else if(valore==3){
+				Icon icona = new ImageIcon(damaBianca);
+				this.setIcon(icona);
+					
+			}
+			else if(valore==4){
+				Icon icona = new ImageIcon(damaNera);
+				this.setIcon(icona);
+			}	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+}
